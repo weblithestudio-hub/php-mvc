db4189b183ce06255129516762c0a7b4823862e0
Madarasi Dániel <mymacair@Madarasi-Air.home>
2026-05-01 13:37:39 +0200

    initializing app file

---
+++ app/init.php
@@ -0,0 +1,22 @@
+<?php
+
+session_start();
+require_once __DIR__ . "/../config/config.php";
+require_once __DIR__ . "/../config/database.php";
+
+spl_autoload_register(function ($class_name) {
+    $paths = [
+        __DIR__ . '/controllers/',
+        __DIR__ . '/models/',
+    ];
+
+    foreach ($paths as $path) {
+        $file = $path . $class_name . '.php';
+
+        if (file_exists($file)) {
+            require_once $file;
+
+            return true;
+        }
+    }
+});
--- app/int.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-session_start();
-require_once __DIR__ . "/../config/config.php";
-require_once __DIR__ . "/../config/database.php";
